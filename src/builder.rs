e0a4f027adc1644033848d2bc9a722a54699bba0
Dragoș Tiselice <dtiselice@google.com>
2022-05-05 16:16:24 +0200

    Updated to fixed version of wgpu.

--- src/builder.rs
+++ src/builder.rs
@@ -529,76 +529,8 @@ const fn get_texture_format_size(texture_format: wgpu::TextureFormat) -> f32 {
         | Etc2Rgb8A1Unorm
         | Etc2Rgb8A1UnormSrgb
         | EacR11Unorm
-        | EacR11Snorm => 0.5, // 4.0 * 4.0 / 8.0
-
-        // 4x4 blocks, 16 bytes per block
-        Bc2RgbaUnorm
-        | Bc2RgbaUnormSrgb
-        | Bc3RgbaUnorm
-        | Bc3RgbaUnormSrgb
-        | Bc5RgUnorm
-        | Bc5RgSnorm
-        | Bc6hRgbUfloat
-        | Bc6hRgbSfloat
-        | Bc7RgbaUnorm
-        | Bc7RgbaUnormSrgb
-        | EacRg11Unorm
-        | EacRg11Snorm
-        | Etc2Rgba8Unorm
-        | Etc2Rgba8UnormSrgb
-        | Astc4x4RgbaUnorm
-        | Astc4x4RgbaUnormSrgb => 1.0, // 4.0 * 4.0 / 16.0
-
-        // 5x4 blocks, 16 bytes per block
-        Astc5x4RgbaUnorm
-        | Astc5x4RgbaUnormSrgb => 1.25, // 5.0 * 4.0 / 16.0
-
-        // 5x5 blocks, 16 bytes per block
-        Astc5x5RgbaUnorm
-        | Astc5x5RgbaUnormSrgb => 1.5625, // 5.0 * 5.0 / 16.0
-
-        // 6x5 blocks, 16 bytes per block
-        Astc6x5RgbaUnorm
-        | Astc6x5RgbaUnormSrgb => 1.875, // 6.0 * 5.0 / 16.0
-
-        // 6x6 blocks, 16 bytes per block
-        Astc6x6RgbaUnorm
-        | Astc6x6RgbaUnormSrgb => 2.25, // 6.0 * 6.0 / 16.0
-
-        // 8x5 blocks, 16 bytes per block
-        Astc8x5RgbaUnorm
-        | Astc8x5RgbaUnormSrgb => 2.5, // 8.0 * 5.0 / 16.0
-
-        // 8x6 blocks, 16 bytes per block
-        Astc8x6RgbaUnorm
-        | Astc8x6RgbaUnormSrgb => 3.0, // 8.0 * 6.0 / 16.0
-
-        // 8x8 blocks, 16 bytes per block
-        Astc8x8RgbaUnorm
-        | Astc8x8RgbaUnormSrgb => 4.0, // 8.0 * 8.0 / 16.0
-
-        // 10x5 blocks, 16 bytes per block
-        Astc10x5RgbaUnorm
-        | Astc10x5RgbaUnormSrgb => 3.125, // 10.0 * 5.0 / 16.0
-
-        // 10x6 blocks, 16 bytes per block
-        Astc10x6RgbaUnorm
-        | Astc10x6RgbaUnormSrgb => 3.75, // 10.0 * 6.0 / 16.0
-
-        // 10x8 blocks, 16 bytes per block
-        Astc10x8RgbaUnorm
-        | Astc10x8RgbaUnormSrgb => 5.0, // 10.0 * 8.0 / 16.0
-
-        // 10x10 blocks, 16 bytes per block
-        Astc10x10RgbaUnorm
-        | Astc10x10RgbaUnormSrgb => 6.25, // 10.0 * 10.0 / 16.0
-
-        // 12x10 blocks, 16 bytes per block
-        Astc12x10RgbaUnorm
-        | Astc12x10RgbaUnormSrgb => 7.5, // 12.0 * 10.0 / 16.0
-
-        // 12x12 blocks, 16 bytes per block
-        Astc12x12RgbaUnorm
-        | Astc12x12RgbaUnormSrgb => 9.0, // 12.0 * 12.0 / 16.0
+        | EacR11Snorm => 0.5,
+        
+        _ => unimplemented!(),
     }
 }
